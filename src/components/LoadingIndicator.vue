d6c5666f26b59283aa2089598aafd2a5a88a3b26
Christian Ankowski <chris.ankowski@gmail.com>
2022-05-31 20:21:51 +0100

    Update LoadingIndicator.vue

--- src/components/LoadingIndicator.vue
+++ src/components/LoadingIndicator.vue
@@ -1,16 +1,16 @@
 <script lang="ts">
 import Vue from "vue";
-import CashfoxHeadIcon from "@/components/icons/LogoSquareIcon.vue";
+import LogoSquareIcon from "@/components/icons/LogoSquareIcon.vue";
 
 export default Vue.extend({
   name: "LoadingIndicator",
-  components: { CashfoxHeadIcon },
+  components: { LogoSquareIcon },
 });
 </script>
 
 <template>
   <div class="h-full flex justify-center items-center">
-    <CashfoxHeadIcon
+    <LogoSquareIcon
       class="mx-auto h-[45px] animate-[spin_5s_linear_infinite] duration-1000 ease-linear"
       fill-class="fill-[#0c5c51]"
     />
